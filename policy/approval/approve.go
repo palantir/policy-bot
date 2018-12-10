23c071cea745e0e0d4afd7557b40759d0abe5530
Javier Campanini <jcampanini@palantir.com>
2018-12-10 14:01:10 -0500

    switch to required

--- policy/approval/approve.go
+++ policy/approval/approve.go
@@ -201,7 +201,7 @@ func (r *Rule) IsApproved(ctx context.Context, prctx pull.Context) (bool, string
 	}
 
 	if len(candidates) > 0 && len(approvers) == 0 {
-		msg := fmt.Sprintf("%d/%d approvals needed. Ignored %s from disqualified users",
+		msg := fmt.Sprintf("%d/%d approvals required. Ignored %s from disqualified users",
 			len(approvers),
 			r.Requires.Count,
 			numberOfApprovals(len(candidates)))
