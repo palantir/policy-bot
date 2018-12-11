dbf68f22899829bb52933416d2dadbed2f3163cf
Billy Keyes <bkeyes@palantir.com>
2018-12-10 16:41:22 -0800

    Add tests for IgnoreUpdateMerges
    Refactor the approval test code to avoid destructively modifying the
    default pull.Context data.

--- policy/approval/approve_test.go
+++ policy/approval/approve_test.go
@@ -34,84 +34,100 @@ func TestIsApproved(t *testing.T) {
 	ctx := logger.WithContext(context.Background())
 
 	now := time.Now()
-	prctx := &pulltest.Context{
-		AuthorValue: "mhaypenny",
-		CommentsValue: []*pull.Comment{
-			{
-				CreatedAt: now.Add(10 * time.Second),
-				Author:    "other-user",
-				Body:      "Why did you do this?",
+	basePullContext := func() *pulltest.Context {
+		return &pulltest.Context{
+			AuthorValue: "mhaypenny",
+			CommentsValue: []*pull.Comment{
+				{
+					CreatedAt: now.Add(10 * time.Second),
+					Author:    "other-user",
+					Body:      "Why did you do this?",
+				},
+				{
+					CreatedAt: now.Add(20 * time.Second),
+					Author:    "comment-approver",
+					Body:      "LGTM :+1: :shipit:",
+				},
+				{
+					CreatedAt: now.Add(30 * time.Second),
+					Author:    "disapprover",
+					Body:      "I don't like things! :-1:",
+				},
+				{
+					CreatedAt: now.Add(40 * time.Second),
+					Author:    "mhaypenny",
+					Body:      ":+1: my stuff is cool",
+				},
+				{
+					CreatedAt: now.Add(50 * time.Second),
+					Author:    "contributor-author",
+					Body:      ":+1: I added to this PR",
+				},
+				{
+					CreatedAt: now.Add(60 * time.Second),
+					Author:    "contributor-committer",
+					Body:      ":+1: I also added to this PR",
+				},
 			},
-			{
-				CreatedAt: now.Add(20 * time.Second),
-				Author:    "comment-approver",
-				Body:      "LGTM :+1: :shipit:",
+			ReviewsValue: []*pull.Review{
+				{
+					CreatedAt: now.Add(70 * time.Second),
+					Author:    "disapprover",
+					State:     pull.ReviewChangesRequested,
+					Body:      "I _really_ don't like things!",
+				},
+				{
+					CreatedAt: now.Add(80 * time.Second),
+					Author:    "review-approver",
+					State:     pull.ReviewApproved,
+					Body:      "I LIKE THIS",
+				},
 			},
-			{
-				CreatedAt: now.Add(30 * time.Second),
-				Author:    "disapprover",
-				Body:      "I don't like things! :-1:",
+			CommitsValue: []*pull.Commit{
+				{
+					CreatedAt: now.Add(5 * time.Second),
+					SHA:       "c6ade256ecfc755d8bc877ef22cc9e01745d46bb",
+					Author:    "mhaypenny",
+					Committer: "mhaypenny",
+				},
+				{
+					CreatedAt: now.Add(15 * time.Second),
+					SHA:       "674832587eaaf416371b30f5bc5a47e377f534ec",
+					Author:    "contributor-author",
+					Committer: "mhaypenny",
+				},
+				{
+					CreatedAt: now.Add(45 * time.Second),
+					SHA:       "97d5ea26da319a987d80f6db0b7ef759f2f2e441",
+					Author:    "mhaypenny",
+					Committer: "contributor-committer",
+				},
 			},
-			{
-				CreatedAt: now.Add(40 * time.Second),
-				Author:    "mhaypenny",
-				Body:      ":+1: my stuff is cool",
+			TargetCommitsValue: []*pull.Commit{
+				{
+					CreatedAt: now.Add(5 * time.Second),
+					SHA:       "dc594ff5aca4133070a76f9006568b656a251770",
+					Author:    "developer",
+					Committer: "developer",
+				},
+				{
+					CreatedAt: now.Add(0 * time.Second),
+					SHA:       "2e1b0bb6ab144bf7a1b7a1df9d3bdcb0fe85a206",
+					Author:    "developer",
+					Committer: "developer",
+				},
 			},
-			{
-				CreatedAt: now.Add(50 * time.Second),
-				Author:    "contributor-author",
-				Body:      ":+1: I added to this PR",
+			OrgMemberships: map[string][]string{
+				"mhaypenny":             {"everyone"},
+				"contributor-author":    {"everyone"},
+				"contributor-committer": {"everyone"},
+				"comment-approver":      {"everyone", "cool-org"},
+				"review-approver":       {"everyone", "even-cooler-org"},
 			},
-			{
-				CreatedAt: now.Add(60 * time.Second),
-				Author:    "contributor-committer",
-				Body:      ":+1: I also added to this PR",
-			},
-		},
-		ReviewsValue: []*pull.Review{
-			{
-				CreatedAt: now.Add(70 * time.Second),
-				Author:    "disapprover",
-				State:     pull.ReviewChangesRequested,
-				Body:      "I _really_ don't like things!",
-			},
-			{
-				CreatedAt: now.Add(80 * time.Second),
-				Author:    "review-approver",
-				State:     pull.ReviewApproved,
-				Body:      "I LIKE THIS",
-			},
-		},
-		CommitsValue: []*pull.Commit{
-			{
-				CreatedAt: now.Add(90 * time.Second),
-				SHA:       "c6ade256ecfc755d8bc877ef22cc9e01745d46bb",
-				Author:    "mhaypenny",
-				Committer: "mhaypenny",
-			},
-			{
-				CreatedAt: now.Add(100 * time.Second),
-				SHA:       "674832587eaaf416371b30f5bc5a47e377f534ec",
-				Author:    "contributor-author",
-				Committer: "mhaypenny",
-			},
-			{
-				CreatedAt: now.Add(110 * time.Second),
-				SHA:       "97d5ea26da319a987d80f6db0b7ef759f2f2e441",
-				Author:    "mhaypenny",
-				Committer: "contributor-committer",
-			},
-		},
-		OrgMemberships: map[string][]string{
-			"mhaypenny":             {"everyone"},
-			"contributor-author":    {"everyone"},
-			"contributor-committer": {"everyone"},
-			"comment-approver":      {"everyone", "cool-org"},
-			"review-approver":       {"everyone", "even-cooler-org"},
-		},
+		}
 	}
 
-	assertApproved := func(t *testing.T, r *Rule, expected string) {
+	assertApproved := func(t *testing.T, prctx pull.Context, r *Rule, expected string) {
 		approved, msg, err := r.IsApproved(ctx, prctx)
 		require.NoError(t, err)
 
@@ -120,7 +136,7 @@ func TestIsApproved(t *testing.T) {
 		}
 	}
 
-	assertPending := func(t *testing.T, r *Rule, expected string) {
+	assertPending := func(t *testing.T, prctx pull.Context, r *Rule, expected string) {
 		approved, msg, err := r.IsApproved(ctx, prctx)
 		require.NoError(t, err)
 
@@ -130,20 +146,23 @@ func TestIsApproved(t *testing.T) {
 	}
 
 	t.Run("noApprovalRequired", func(t *testing.T) {
+		prctx := basePullContext()
 		r := &Rule{}
-		assertApproved(t, r, "No approval required")
+		assertApproved(t, prctx, r, "No approval required")
 	})
 
 	t.Run("singleApprovalRequired", func(t *testing.T) {
+		prctx := basePullContext()
 		r := &Rule{
 			Requires: Requires{
 				Count: 1,
 			},
 		}
-		assertPending(t, r, "0/1 approvals required. Ignored 5 approvals from disqualified users")
+		assertPending(t, prctx, r, "0/1 approvals required. Ignored 5 approvals from disqualified users")
 	})
 
 	t.Run("authorCannotApprove", func(t *testing.T) {
+		prctx := basePullContext()
 		r := &Rule{
 			Options: Options{
 				AllowAuthor: false,
@@ -155,10 +174,11 @@ func TestIsApproved(t *testing.T) {
 				},
 			},
 		}
-		assertApproved(t, r, "Approved by comment-approver, review-approver")
+		assertApproved(t, prctx, r, "Approved by comment-approver, review-approver")
 	})
 
 	t.Run("contributorsCannotApprove", func(t *testing.T) {
+		prctx := basePullContext()
 		r := &Rule{
 			Options: Options{
 				AllowContributor: false,
@@ -170,10 +190,11 @@ func TestIsApproved(t *testing.T) {
 				},
 			},
 		}
-		assertApproved(t, r, "Approved by comment-approver, review-approver")
+		assertApproved(t, prctx, r, "Approved by comment-approver, review-approver")
 	})
 
 	t.Run("contributorsCanApprove", func(t *testing.T) {
+		prctx := basePullContext()
 		r := &Rule{
 			Options: Options{
 				AllowContributor: true,
@@ -185,10 +206,11 @@ func TestIsApproved(t *testing.T) {
 				},
 			},
 		}
-		assertApproved(t, r, "Approved by comment-approver, mhaypenny, contributor-author, contributor-committer, review-approver")
+		assertApproved(t, prctx, r, "Approved by comment-approver, mhaypenny, contributor-author, contributor-committer, review-approver")
 	})
 
 	t.Run("specificUserApproves", func(t *testing.T) {
+		prctx := basePullContext()
 		r := &Rule{
 			Requires: Requires{
 				Count: 1,
@@ -197,7 +219,7 @@ func TestIsApproved(t *testing.T) {
 				},
 			},
 		}
-		assertApproved(t, r, "Approved by comment-approver")
+		assertApproved(t, prctx, r, "Approved by comment-approver")
 
 		r = &Rule{
 			Requires: Requires{
@@ -207,10 +229,11 @@ func TestIsApproved(t *testing.T) {
 				},
 			},
 		}
-		assertPending(t, r, "0/1 approvals required. Ignored 5 approvals from disqualified users")
+		assertPending(t, prctx, r, "0/1 approvals required. Ignored 5 approvals from disqualified users")
 	})
 
 	t.Run("specificOrgApproves", func(t *testing.T) {
+		prctx := basePullContext()
 		r := &Rule{
 			Requires: Requires{
 				Count: 1,
@@ -219,7 +242,7 @@ func TestIsApproved(t *testing.T) {
 				},
 			},
 		}
-		assertApproved(t, r, "Approved by comment-approver")
+		assertApproved(t, prctx, r, "Approved by comment-approver")
 
 		r = &Rule{
 			Requires: Requires{
@@ -229,10 +252,11 @@ func TestIsApproved(t *testing.T) {
 				},
 			},
 		}
-		assertPending(t, r, "0/1 approvals required. Ignored 5 approvals from disqualified users")
+		assertPending(t, prctx, r, "0/1 approvals required. Ignored 5 approvals from disqualified users")
 	})
 
 	t.Run("specificOrgsOrUserApproves", func(t *testing.T) {
+		prctx := basePullContext()
 		r := &Rule{
 			Requires: Requires{
 				Count: 2,
@@ -242,12 +266,14 @@ func TestIsApproved(t *testing.T) {
 				},
 			},
 		}
-		assertApproved(t, r, "Approved by comment-approver, review-approver")
+		assertApproved(t, prctx, r, "Approved by comment-approver, review-approver")
 	})
 
-	t.Run("invalidateOnPush_comment", func(t *testing.T) {
+	t.Run("invalidateCommentOnPush", func(t *testing.T) {
+		prctx := basePullContext()
 		prctx.CommitsValue = []*pull.Commit{
 			{
+				CreatedAt: now.Add(25 * time.Second),
 				SHA:       "c6ade256ecfc755d8bc877ef22cc9e01745d46bb",
 				Author:    "mhaypenny",
 				Committer: "mhaypenny",
@@ -261,23 +287,18 @@ func TestIsApproved(t *testing.T) {
 					Users: []string{"comment-approver"},
 				},
 			},
-			Options: Options{
-				InvalidateOnPush: true,
-			},
 		}
+		assertApproved(t, prctx, r, "Approved by comment-approver")
 
-		// set the commit after the comment
-		prctx.CommitsValue[0].CreatedAt = now.Add(25 * time.Second)
-		assertPending(t, r, "0/1 approvals required. Ignored 4 approvals from disqualified users")
-
-		// set the commit before the comment
-		prctx.CommitsValue[0].CreatedAt = now.Add(15 * time.Second)
-		assertApproved(t, r, "Approved by comment-approver")
+		r.Options.InvalidateOnPush = true
+		assertPending(t, prctx, r, "0/1 approvals required. Ignored 4 approvals from disqualified users")
 	})
 
-	t.Run("invalidateOnPush_review", func(t *testing.T) {
+	t.Run("invalidateReviewOnPush", func(t *testing.T) {
+		prctx := basePullContext()
 		prctx.CommitsValue = []*pull.Commit{
 			{
+				CreatedAt: now.Add(85 * time.Second),
 				SHA:       "c6ade256ecfc755d8bc877ef22cc9e01745d46bb",
 				Author:    "mhaypenny",
 				Committer: "mhaypenny",
@@ -291,17 +312,72 @@ func TestIsApproved(t *testing.T) {
 					Users: []string{"review-approver"},
 				},
 			},
+		}
+		assertApproved(t, prctx, r, "Approved by review-approver")
+
+		r.Options.InvalidateOnPush = true
+		assertPending(t, prctx, r, "0/1 approvals required")
+	})
+
+	t.Run("ignoreUpdateMergeAfterReview", func(t *testing.T) {
+		prctx := basePullContext()
+		prctx.CommitsValue = append(prctx.CommitsValue[:1], &pull.Commit{
+			CreatedAt:       now.Add(25 * time.Second),
+			SHA:             "647c5078288f0ea9de27b5c280f25edaf2089045",
+			CommittedViaWeb: true,
+			Parents: []string{
+				"c6ade256ecfc755d8bc877ef22cc9e01745d46bb",
+				"2e1b0bb6ab144bf7a1b7a1df9d3bdcb0fe85a206",
+			},
+			Author: "merge-committer",
+		})
+
+		r := &Rule{
+			Requires: Requires{
+				Count: 1,
+				Actors: common.Actors{
+					Users: []string{"comment-approver"},
+				},
+			},
 			Options: Options{
 				InvalidateOnPush: true,
 			},
 		}
+		assertPending(t, prctx, r, "0/1 approvals required. Ignored 4 approvals from disqualified users")
 
-		// set the commit after the review
-		prctx.CommitsValue[0].CreatedAt = now.Add(85 * time.Second)
-		assertPending(t, r, "0/1 approvals required")
+		r.Options.IgnoreUpdateMerges = true
+		assertApproved(t, prctx, r, "Approved by comment-approver")
+	})
 
-		// set the commit before the review
-		prctx.CommitsValue[0].CreatedAt = now.Add(75 * time.Second)
-		assertApproved(t, r, "Approved by review-approver")
+	t.Run("ignoreUpdateMergeContributor", func(t *testing.T) {
+		prctx := basePullContext()
+		prctx.CommitsValue = append(prctx.CommitsValue[:1], &pull.Commit{
+			CreatedAt:       now.Add(25 * time.Second),
+			SHA:             "647c5078288f0ea9de27b5c280f25edaf2089045",
+			CommittedViaWeb: true,
+			Parents: []string{
+				"c6ade256ecfc755d8bc877ef22cc9e01745d46bb",
+				"2e1b0bb6ab144bf7a1b7a1df9d3bdcb0fe85a206",
+			},
+			Author: "merge-committer",
+		})
+		prctx.CommentsValue = append(prctx.CommentsValue, &pull.Comment{
+			CreatedAt: now.Add(100 * time.Second),
+			Author:    "merge-committer",
+			Body:      ":+1:",
+		})
+
+		r := &Rule{
+			Requires: Requires{
+				Count: 1,
+				Actors: common.Actors{
+					Users: []string{"merge-committer"},
+				},
+			},
+		}
+		assertPending(t, prctx, r, "0/1 approvals required. Ignored 6 approvals from disqualified users")
+
+		r.Options.IgnoreUpdateMerges = true
+		assertApproved(t, prctx, r, "Approved by merge-committer")
 	})
 }
