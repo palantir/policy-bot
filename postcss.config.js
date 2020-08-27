0c11b6f633b56a04a39ec06b58bd601fc56915a2
Billy Keyes <bkeyes@palantir.com>
2020-08-27 13:26:26 -0700

    Upgrade Tailwind to v1.7.5 from v0.6.6
    As part of this, follow the new configuration best-practice to only
    include override values. I think I accounted for all the breaking
    changes, but there may be some lingering visual weirdness.
    
    Also upgrade other build dependencies to the latest version.

--- postcss.config.js
+++ postcss.config.js
@@ -4,26 +4,20 @@ const cssnano = require('cssnano');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-// from https://tailwindcss.com/docs/controlling-file-size#removing-unused-css-with-purgecss
-// needed to handle special characters in class names
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-  }
-}
-
 module.exports = {
   plugins: [
     tailwind('./tailwind.config.js'),
     ...(isProduction ? [
+      // https://tailwindcss.com/docs/controlling-file-size#setting-up-purge-css-manually
       purgecss({
         content: [
           './server/templates/**/*.html.tmpl',
         ],
-        extractors: [{
-          extractor: TailwindExtractor,
-          extensions: ["html.tmpl"],
-        }],
+        defaultExtractor: content => {
+          const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+          const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+          return broadMatches.concat(innerMatches)
+        },
         // status classes are dynamically generated
         whitelist: [
           "approved", "disapproved", "pending", "skipped", "error",
