aca9e41546f1ede364c580baad31c4e78f50a4cc
asvoboda <svoboda.andrew@gmail.com>
2024-01-08 13:13:13 +0000

    Update go-github to v57

--- pull/list_teams.go
+++ pull/list_teams.go
@@ -20,7 +20,7 @@ import (
 	"net/url"
 	"reflect"
 
-	"github.com/google/go-github/v56/github"
+	"github.com/google/go-github/v57/github"
 	"github.com/google/go-querystring/query"
 )
 
--- server/handler/eval_context.go
+++ server/handler/eval_context.go
@@ -19,7 +19,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/google/go-github/v56/github"
+	"github.com/google/go-github/v57/github"
 	"github.com/palantir/policy-bot/policy"
 	"github.com/palantir/policy-bot/policy/common"
 	"github.com/palantir/policy-bot/pull"
