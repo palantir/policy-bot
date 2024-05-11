f3f163578a06c066f70b3aa14483b6396aff8951
asvoboda <svoboda.andrew@gmail.com>
2024-05-11 16:05:20 +0100

    Update go-github to v61

--- server/handler/installation.go
+++ server/handler/installation.go
@@ -20,7 +20,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/google/go-github/v60/github"
+	"github.com/google/go-github/v61/github"
 	"github.com/palantir/go-githubapp/githubapp"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
