94e91741e9da41d8f228f45d83ddc77411f664d0
Billy Keyes <commits@billykeyes.dev>
2024-07-24 21:38:58 -0700

    Fix accidental use of go-github v62
    This was introduced by a PR that was created before the upgrade to v63,
    but merged afterwards.

--- policy/predicate/status_test.go
+++ policy/predicate/status_test.go
@@ -21,7 +21,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/google/go-github/v62/github"
+	"github.com/google/go-github/v63/github"
 	"github.com/palantir/policy-bot/policy/common"
 	"github.com/palantir/policy-bot/pull"
 	"github.com/palantir/policy-bot/pull/pulltest"
