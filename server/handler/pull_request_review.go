fc09d7a6bbe0e21327785496912e7eb39770ed93
Alan Patel <alankpatel@gmail.com>
2024-10-23 19:40:29 -0700

    Add dismissed state (#866)

--- server/handler/pull_request_review.go
+++ server/handler/pull_request_review.go
@@ -88,6 +88,10 @@ func (h *PullRequestReview) Handle(ctx context.Context, eventType, deliveryID st
 
 func (h *PullRequestReview) affectsApproval(reviewState pull.ReviewState, config *policy.Config) bool {
 	states := make(map[pull.ReviewState]struct{})
+
+	// Always process events for dismissed reviews because they can revert the overall approval or disapproval to a previous state
+	states[pull.ReviewDismissed] = struct{}{}
+
 	for _, rule := range config.ApprovalRules {
 		states[rule.Options.GetMethods().GithubReviewState] = struct{}{}
 	}
