dcf72790cb024c2666b6b73a74189b6b54f1669a
asvoboda <asvoboda@palantir.com>
2025-06-24 12:46:18 +0100

    Update commentrlh

--- policy/simulated/options.go
+++ policy/simulated/options.go
@@ -72,7 +72,7 @@ type Comment struct {
 	Body         string     `json:"body"`
 }
 
-// setDefaults sets the createdAt and lastEdtedAt values to time.Now() if they are otherwise unset
+// setDefaults sets the createdAt value to time.Now(), and the lastEditedAt value to zero, if they are otherwise unset
 func (c *Comment) setDefaults() {
 	now := time.Now()
 	if c.CreatedAt == nil {
